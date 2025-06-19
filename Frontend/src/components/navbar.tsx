da4da9db0dadc55db26a1ff35fbd621cb9c8813f
DankmemerXDD <kims9725@hotmail.com>
2025-06-19 14:00:55 +0200

    Basic components
    About, home, Store.
    Added navigation between them

---
+++ Frontend/src/components/navbar.tsx
@@ -0,0 +1,12 @@
+import { Navbar as NavbarBootStrap } from "react-bootstrap"
+
+export function Navbar() {
+    return (
+    <NavbarBootStrap className="navBackground">
+        <Container> Nav</Container>
+        </NavbarBootStrap>
+
+
+
+    )
+}
